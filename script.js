d977447af1cf14ad0f1010c5c14d951002733f85
Danbka-Taranbka <danila.klim01@gmail.com>
2023-01-16 18:05:18 +0300

    no-repeat random

--- script.js
+++ script.js
@@ -5,16 +5,32 @@ let actionArray = [
   'Сходить на каток',
   'Погулять',
   'Сходить в ресторан',
-  'Сходить в кино'
+  'Сходить в кино',
+  'Поиграть в настольные игры',
+  'Поиграть в видеоигры',
+  'Вместе потренироваться', 
+  'Сходить в красивое место и пофотографироваться', 
+  'Почитать книгу', 
+  'Притвориться англичанами', 
+  'Заняться рукоделием', 
+  'Сходить в театр', 
+  'Вместе приготовить блюдо'
 ];
 
+let previousNumber = 0;
+
 function getRandom (array) {
+  const difference = array.length;
+  let rand = Math.floor(Math.random() * difference);
   
-  let difference = array.length - 0;
-  let rand = Math.random();
-  rand = Math.floor(rand * difference);
+  while (previousNumber === rand) {
+    rand = Math.floor(Math.random() * difference);
+  }  
+
   let act = array[rand];
   pasteText(action, act);
+
+  previousNumber = rand;
 }
 
 function pasteText (paragraph, text) {
